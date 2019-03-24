daa23c89ac1dd07b3b9233003c50f8373322f085
Axel Fernández <axl1210@estudiantec.cr>
2019-03-23 20:03:52 -0600

    Login
    Se eliminó casi todo el contenido, a excepción del logo y el significado de las siglas pasa a ser explicado. MYSS significa My Social Space.

--- myss/login.php
+++ myss/login.php
@@ -2,10 +2,7 @@
 <html lang="zxx" class="no-js">
 <head>
 
-    <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
     <link rel="shortcut icon" href="img/fav.png" />
     <meta name="author" content="CodePixar" />
@@ -18,7 +15,6 @@
             href="https://fonts.googleapis.com/css?family=Open+Sans:400,600|Playfair+Display:700,700i"
             rel="stylesheet"
     />
-
     <link rel="stylesheet" href="css/linearicons.css" />
     <link rel="stylesheet" href="css/font-awesome.min.css" />
     <link rel="stylesheet" href="css/magnific-popup.css" />
@@ -31,377 +27,27 @@
 </head>
 
 <body>
-<!--================ Start Header Area =================-->
-<header class="header-area">
-    <div class="container">
-        <div class="header-wrap">
-            <div class="header-top d-flex justify-content-between align-items-lg-center navbar-expand-lg">
-                <div class="col menu-left">
-                    <a class="active" href="index.php">Home</a>
-                    <a class="active" href="profile.php">Profile</a>
-                </div>
-                <div class="col-5 text-lg-center mt-2 mt-lg-0">
-              <span class="logo-outer">
-                <span class="logo-inner">
-                  <a href="index.php"><img class="mx-auto" src="img/logo.png" alt=""/></a>
-                </span>
-              </span>
-                </div>
-                <nav class="col navbar navbar-expand-lg justify-content-end">
+<div align="center">
 
-                    <!-- Toggler/collapsibe Button -->
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                        <span class="lnr lnr-menu"></span>
-                    </button>
-
-                    <!-- Navbar links -->
-                    <div class="collapse navbar-collapse menu-right" id="collapsibleNavbar">
-                        <ul class="navbar-nav justify-content-center w-100">
-                            <li class="nav-item hide-lg">
-                                <a class="nav-link" href="index.php">Home</a>
-                            </li>
-                            <li class="nav-item hide-lg">
-                                <a class="nav-link" href="tag.php">Category</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="settings.php">Settings</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="logout.php">Logout</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </div>
+    <div class="wrap-login100 p-t-190 p-b-30">
+    <span class="logo-outer" >
+        <span class="logo-inner">
+          <a href="index.php"><img class="mx-auto" src="img/logo.png" alt=""/></a>
+        </span>
+    </span>
     </div>
-</header>
-<!--================ End Header Area =================-->
-
-<!--================ Start Blog Post Area =================-->
-<section class="blog-post-area ">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="single-amenities">
-                            <div class="amenities-details">
-                                <h5>
-                                    <a href="#"> Text Post... </a>
-                                </h5>
-                                <div class="amenities-meta mb-10">
-                                    <a href="#" class="ti-user"> Username </a>
-                                    <a href="#" class=""><span class="ti-alarm-clock"></span> Post Time </a>
-                                </div>
-
-                                <div class="d-flex justify-content-between mt-20">
-                                    <div>
-                                        <a href="#" class="blog-post-btn">
-                                            # Comments <span class="ti-comment"></span>
-                                        </a>
-                                    </div>
-
-                                    <div class="tag_btn">
-                                        <a href="#">
-                                            <span class="ti-tag mr-1"></span> Tag
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="single-amenities">
-                            <div class="amenities-details">
-                                <h5>
-                                    <a href="#"> Text Post... </a>
-                                </h5>
-                                <div class="amenities-meta mb-10">
-                                    <a href="#" class="ti-user"> Username </a>
-                                    <a href="#" class=""><span class="ti-alarm-clock"></span> Post Time </a>
-                                </div>
-
-                                <div class="d-flex justify-content-between mt-20">
-                                    <div>
-                                        <a href="#" class="blog-post-btn">
-                                            # Comments <span class="ti-comment"></span>
-                                        </a>
-                                    </div>
-
-                                    <div class="tag_btn">
-                                        <a href="#">
-                                            <span class="ti-tag mr-1"></span> Tag
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="single-amenities">
-                            <div class="amenities-details">
-                                <h5>
-                                    <a href="#"> Text Post... </a>
-                                </h5>
-                                <div class="amenities-meta mb-10">
-                                    <a href="#" class="ti-user"> Username </a>
-                                    <a href="#" class=""><span class="ti-alarm-clock"></span> Post Time </a>
-                                </div>
-
-                                <div class="d-flex justify-content-between mt-20">
-                                    <div>
-                                        <a href="#" class="blog-post-btn">
-                                            # Comments <span class="ti-comment"></span>
-                                        </a>
-                                    </div>
-
-                                    <div class="tag_btn">
-                                        <a href="#">
-                                            <span class="ti-tag mr-1"></span> Tag
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="single-amenities">
-                            <div class="amenities-details">
-                                <h5>
-                                    <a href="#"> Text Post... </a>
-                                </h5>
-                                <div class="amenities-meta mb-10">
-                                    <a href="#" class="ti-user"> Username </a>
-                                    <a href="#" class=""><span class="ti-alarm-clock"></span> Post Time </a>
-                                </div>
-
-                                <div class="d-flex justify-content-between mt-20">
-                                    <div>
-                                        <a href="#" class="blog-post-btn">
-                                            # Comments <span class="ti-comment"></span>
-                                        </a>
-                                    </div>
-
-                                    <div class="tag_btn">
-                                        <a href="#">
-                                            <span class="ti-tag mr-1"></span> Tag
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-6 col-md-12">
-                        <div class="single-amenities">
-                            <div class="amenities-details">
-                                <h5>
-                                    <a href="#"> Text Post... </a>
-                                </h5>
-                                <div class="amenities-meta mb-10">
-                                    <a href="#" class="ti-user"> Username </a>
-                                    <a href="#" class=""><span class="ti-alarm-clock"></span> Post Time </a>
-                                </div>
-
-                                <div class="d-flex justify-content-between mt-20">
-                                    <div>
-                                        <a href="#" class="blog-post-btn">
-                                            # Comments <span class="ti-comment"></span>
-                                        </a>
-                                    </div>
-
-                                    <div class="tag_btn">
-                                        <a href="#">
-                                            <span class="ti-tag mr-1"></span> Tag
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="single-amenities">
-                            <div class="amenities-details">
-                                <h5>
-                                    <a href="#"> Text Post... </a>
-                                </h5>
-                                <div class="amenities-meta mb-10">
-                                    <a href="#" class="ti-user"> Username </a>
-                                    <a href="#" class=""><span class="ti-alarm-clock"></span> Post Time </a>
-                                </div>
-
-                                <div class="d-flex justify-content-between mt-20">
-                                    <div>
-                                        <a href="#" class="blog-post-btn">
-                                            # Comments <span class="ti-comment"></span>
-                                        </a>
-                                    </div>
-
-                                    <div class="tag_btn">
-                                        <a href="#">
-                                            <span class="ti-tag mr-1"></span> Tag
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="single-amenities">
-                            <div class="amenities-details">
-                                <h5>
-                                    <a href="#"> Text Post... </a>
-                                </h5>
-                                <div class="amenities-meta mb-10">
-                                    <a href="#" class="ti-user"> Username </a>
-                                    <a href="#" class=""><span class="ti-alarm-clock"></span> Post Time </a>
-                                </div>
-
-                                <div class="d-flex justify-content-between mt-20">
-                                    <div>
-                                        <a href="#" class="blog-post-btn">
-                                            # Comments <span class="ti-comment"></span>
-                                        </a>
-                                    </div>
-
-                                    <div class="tag_btn">
-                                        <a href="#">
-                                            <span class="ti-tag mr-1"></span> Tag
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="single-amenities">
-                            <div class="amenities-details">
-                                <h5>
-                                    <a href="#"> Text Post... </a>
-                                </h5>
-                                <div class="amenities-meta mb-10">
-                                    <a href="#" class="ti-user"> Username </a>
-                                    <a href="#" class=""><span class="ti-alarm-clock"></span> Post Time </a>
-                                </div>
-
-                                <div class="d-flex justify-content-between mt-20">
-                                    <div>
-                                        <a href="#" class="blog-post-btn">
-                                            # Comments <span class="ti-comment"></span>
-                                        </a>
-                                    </div>
-
-                                    <div class="tag_btn">
-                                        <a href="#">
-                                            <span class="ti-tag mr-1"></span> Tag
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-lg-12">
-                        <nav class="blog-pagination justify-content-center d-flex">
-                            <ul class="pagination">
-                                <li class="page-item">
-                                    <a href="#" class="page-link" aria-label="Previous">
-                                  <span aria-hidden="true">
-                                      <span class="ti-arrow-left"></span>
-                                  </span>
-                                    </a>
-                                </li>
-                                <li class="page-item active"><a href="#" class="page-link">01</a></li>
-                                <li class="page-item"><a href="#" class="page-link">02</a></li>
-                                <li class="page-item"><a href="#" class="page-link">03</a></li>
-                                <li class="page-item"><a href="#" class="page-link">04</a></li>
-                                <li class="page-item"><a href="#" class="page-link">...</a></li>
-                                <li class="page-item">
-                                    <a href="#" class="page-link" aria-label="Next">
-                                  <span aria-hidden="true">
-                                      <span class="ti-arrow-right"></span>
-                                  </span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Start Blog Post Sidebar -->
-            <div class="col-lg-4 sidebar-widgets">
-                <div class="widget-wrap">
-
-                    <!-- Search Bar -->
-                    <div class="single-sidebar-widget search-widget">
-                        <form class="search-form" action="#">
-                            <input placeholder="Search Posts" name="search" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search Posts'">
-                            <button type="submit"><i class="fa fa-search"></i></button>
-                        </form>
-                    </div>
-
-                    <div class="single-sidebar-widget post-category-widget">
-                        <h4 class="category-title ti-tag"> Popular Tags </h4>
-                        <ul class="cat-list mt-20">
-                            <li>
-                                <a href="#" class="d-flex justify-content-between">
-                                    <p>Fashion</p>
-                                    <p>59</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="d-flex justify-content-between">
-                                    <p>Travel</p>
-                                    <p>09</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="d-flex justify-content-between">
-                                    <p>Lifestyle</p>
-                                    <p>24</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="d-flex justify-content-between">
-                                    <p>Shopping</p>
-                                    <p>44</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="d-flex justify-content-between">
-                                    <p>Food</p>
-                                    <p>15</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        <!-- End Blog Post Siddebar -->
+        <span>
+            My Social Space
+        </span>
+    <div>
+        <span>
+            building login...
+        </span>
     </div>
-    </div>
-</section>
-<!--================ End Blog Post Area =================-->
 
-<script src="js/vendor/jquery-2.2.4.min.js"></script>
-<script
-        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
-        integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
-        crossorigin="anonymous"
-></script>
-<script src="js/vendor/bootstrap.min.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/jquery.sticky.js"></script>
-<script src="js/jquery.tabs.min.js"></script>
-<script src="js/parallax.min.js"></script>
-<script src="js/jquery.nice-select.min.js"></script>
-<script src="js/jquery.ajaxchimp.min.js"></script>
-<script src="js/jquery.magnific-popup.min.js"></script>
-<script
-        type="text/javascript"
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"
-></script>
-<script src="js/bootstrap-datepicker.js"></script>
-<script src="js/main.js"></script>
+
+
+</div>
+
 </body>
 </html>
