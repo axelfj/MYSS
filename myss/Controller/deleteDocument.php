261aeb6c44c20358316a518aeb307021029ef27d
Axel Fernández <axl1210@estudiantec.cr>
2019-03-24 19:21:43 -0600

    CRUD
    Se desarrolla el deleteDocument, readDocument y updateDocument.
    
    De momento solo funciona el readDocument.

--- myss/Controller/deleteDocument.php
+++ myss/Controller/deleteDocument.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: azzefj
- * Date: 3/24/2019
- * Time: 6:03 PM
- */
+use ArangoDBClient\Statement;
+
+require_once("connection.php");
+$connection = connect();
+
+// delete document via AQL
+$query = "FOR x IN @collection FILTER x.d == 'qux' REMOVE x IN @@collection RETURN OLD";
+try {
+    $statement = new Statement(
+        $connection,
+        array(
+            "query" => $query,
+            "count" => true,
+            "batchSize" => 1,
+            "sanitize" => true,
+            "bindVars" => array("@collection" => "firstCollection")
+        )
+    );
+    $cursor = $statement->execute();
+    var_dump($cursor);
+} catch (\ArangoDBClient\Exception $e) {
+    echo 'Error.';
+}
+
--- myss/Controller/readDocument.php
+++ myss/Controller/readDocument.php
@@ -1,7 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: azzefj
- * Date: 3/24/2019
- * Time: 6:03 PM
- */
+use ArangoDBClient\Statement;
+
+require_once("connection.php");
+$connection = connect();
+
+// execute AQL queries
+$query = 'FOR x IN user RETURN x.name';
+// user => nombre de colección a la que se le piden datos //
+
+try {
+    $statement = new Statement(
+        $connection,
+        array(
+            "query" => $query,
+            "count" => true,
+            "batchSize" => 1000,
+            "sanitize" => true
+        )
+    );
+    $cursor = $statement->execute();
+    $resultingDocuments = array();
+
+    foreach ($cursor as $key => $value) {
+        $resultingDocuments[$key] = $value;
+    }
+    var_dump($resultingDocuments);
+} catch (\ArangoDBClient\Exception $e) {
+    echo 'Error.';
+}
+
