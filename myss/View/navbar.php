2a172776ff46d3c1047a023d1009cce832f547c1
JuanEscobar <juan066@hotmail.es>
2019-03-25 01:52:38 -0600

    Otros cambios en la GUI
    - Se agregaron íconos en el navbar.
    - Se cambia el color al pasar sobre los ítems.
    - Se arregla el problema de que la navbar no estaba fija en la parte superior de la ventana.

--- myss/View/navbar.php
+++ myss/View/navbar.php
@@ -1,39 +1,30 @@
-<body style="background-color:#EAEAEA;">
+<body style="background-color: #E7E7E9;">
     <!--================ Start Header Area =================-->
     <header class="">
         
             <nav class="navbar navbar-expand-lg navbar-light bg-light static-navbar">
-                <a class="navbar-brand" href="#">MYSS</a>
+                <a style="padding-left:50%;"></a>
+                <a class="navbar-brand" href="index.php" style="color:rgb(56, 164, 255);" onmouseover="this.style.color='rgb(0, 138, 255)';" onmouseout="this.style.color='rgb(56, 164, 255)'">MYSS</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent" style="padding-left:11%;">
+                    <ul class="navbar-nav mr-auto">                                                                                                                   
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" onmouseover="this.style.color='rgb(56, 164, 255)'" onmouseout="this.style.color='';"><i class="fas fa-plus"></i><span class="badge badge-notify"></span> New post</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Profile</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
+                            <a class="nav-link" href="#" onmouseover="this.style.color='rgb(56, 164, 255)'" onmouseout="this.style.color='';"><i class="fas fa-bell"></i><span class="badge badge-notify"></span> Notifications</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Disabled</a>
+                            <a class="nav-link" href="profile.php" onmouseover="this.style.color='rgb(56, 164, 255)'" onmouseout="this.style.color='';"><i class="fas fa-user-alt"></i><span class="badge badge-notify"></span> Profile</a>
                         </li>
-                    </ul>
-                    
+                    </ul>                    
                 </div>
             </nav>
         
     </header><br><br><br><br>
     <!--================ End Header Area =================-->
+    
+    
