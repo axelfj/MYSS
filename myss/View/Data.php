982eb4c11d9c66633cde182798529762bec89da2
Axel Fernández <axl1210@estudiantec.cr>
2019-03-25 22:26:16 -0600

    Scrolldown & Data
    La función de scrolldown ya funciona con los datos, y se cambiaron apropiadamente para realizar pruebas.

--- myss/View/Data.php
+++ myss/View/Data.php
@@ -1,36 +1,40 @@
 <?php
-class Video {
-    public $videoName = "";
-    public $videoViews = "";
-    public function __construct($videoName, $videoViews) {
-        $this->videoName = $videoName;
-        $this->videoViews = $videoViews;
+class Post {
+    public $titlePost = "";
+    public $descriptionPost = "";
+    public $username = "";
+    public $postTime = "";
+    public $tag = "";
+    public function __construct($titlePost, $descriptionPost, $username, $postTime, $tag) {
+        $this->titlePost = $titlePost;
+        $this->descriptionPost = $descriptionPost;
+        $this->username = $username;
+        $this->postTime = $postTime;
+        $this->tag = $tag;
     }
 }
 $start = $_GET['start'];
 $data = array();
-$possibleVideos = array(
-    new Video("How to Make a Social Network from Scratch: Part 1 - Begining the Design (remake)","238,284"),
-    new Video("Send SMS Messages With PHP", "117,328"),
-    new Video("How to Make a Social Network from Scratch: Part 42 - Deleting Photos & Albums", "94,256"),
-    new Video("How to Make a Social Network from Scratch: Part 2 - Editing the Design (remake)", "66,012"),
-    new Video("How to Make a Social Network from Scratch: Part 6 - Creating the Database (remake)", "56,959"),
-    new Video("How to Make a Social Network from Scratch: Part 3 - Adding a Search Box (remake)", "54,128"),
-    new Video("Make Your Own Programming Language - Part 1 - Lexer", "48,842"),
-    new Video("How to Make a Social Network from Scratch: Part 4 - Creating the Menu (remake)", "47,109"),
-    new Video("Make your Own Facebook Bot - Part 1 - Setting Up", "45,555"),
-    new Video("How to Make a Social Network from Scratch: Part 5 - Designing the Homepage (remake)", "44,817"),
-    new Video("How to Make a Social Network from Scratch: Part 7 - Making the Register form work (remake)", "40,151"),
-    new Video("Python 101 - Making a Window", "35,017"),
-    new Video("Make Your Own Chrome Extension: Part 1", "34,324"),
-    new Video("How to Make a Social Network from Scratch: Part 8 - Creating the User Login (remake)", "32,561"),
-    new Video("Make Your Own Programming Language - Introduction", "31,878"),
-    new Video("Python 101 - Open File Dialog", "28,794"),
-    new Video("Python 101 - Creating a Menu", "25,604")
+$possiblePost = array(
+    new Post("Prueba #1","Descripción de prueba", "axl1210", "12:10", "YOLO"),
+    new Post("Prueba #2","Descripción de prueba", "juancho", "12:10", "YOLO"),
+    new Post("Prueba #3","Descripción de prueba", "mapachurro", "12:10", "YOLO"),
+    new Post("Prueba #4","Descripción de prueba", "aobm", "12:10", "YOLO"),
+    new Post("Prueba #5","Descripción de prueba", "azzefj", "12:10", "YOLO"),
+    new Post("Prueba #6","Descripción de prueba", "axl1210", "12:10", "YOLO"),
+    new Post("Prueba #7","Descripción de prueba", "axl1210", "12:10", "YOLO"),
+    new Post("Prueba #8","Descripción de prueba", "axl1210", "12:10", "YOLO"),
+    new Post("Prueba #9","Descripción de prueba", "axl1210", "12:10", "YOLO"),
+    new Post("Prueba #10","Descripción de prueba", "axl1210", "12:10", "YOLO"),
+    new Post("Prueba #11","Descripción de prueba", "axl1210", "12:10", "YOLO"),
+    new Post("Prueba #12","Descripción de prueba", "axl1210", "12:10", "YOLO"),
+    new Post("Prueba #13","Descripción de prueba", "axl1210", "12:10", "YOLO"),
+    new Post("Prueba #14","Descripción de prueba", "axl1210", "12:10", "YOLO"),
+    new Post("Prueba #15","Descripción de prueba", "axl1210", "12:10", "YOLO")
 );
 for ($i = $start; $i < $start+5; $i++) {
-    if ($i < count($possibleVideos)) {
-        array_push($data, $possibleVideos[$i]);
+    if ($i < count($possiblePost)) {
+        array_push($data, $possiblePost[$i]);
     }
 }
 //echo "<pre>";
