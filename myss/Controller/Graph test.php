2ddd9056f51e0723fc3d0a4453a295b4dc0985e9
Brends31 <aobm98@gmail.com>
2019-04-01 19:05:01 -0600

    Deletition of useless lines
    Eliminé lineas inutiles

--- myss/Controller/Graph test.php
+++ myss/Controller/Graph test.php
@@ -5,14 +5,3 @@
  * Date: 25/03/2019
  * Time: 22:58
  */
-
-ArangoDB arango = new ArangoDB.Builder().build();
-ArangoDatabase db = arango.db("myDB");
-
-EdgeDefinition edgeDefinition = new EdgeDefinition()
-    .collection("edges")
-    .from("start-vertices")
-    .to("end-vertices");
-GraphEntity graph = db.createGraph(
-        "some-graph", Arrays.asList(edgeDefinition), new GraphCreateOptions()
-    );
