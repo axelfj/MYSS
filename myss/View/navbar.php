8e18b1eef32a8bc379ac7f2057709efcbc8ca50f
Axel Fernández <axl1210@estudiantec.cr>
2019-04-05 09:45:05 -0600

    Navbar centered
    Se centraron los textos y los botones de acuerdo al index y al profile.

--- myss/View/navbar.php
+++ myss/View/navbar.php
@@ -2,7 +2,7 @@
 <!--================ Start Header Area =================-->
 <header class="">
     <nav class="navbar navbar-expand-lg navbar-light bg-light static-navbar justify-content-end">
-        <a class="navbar-brand" style="color:rgb(56, 164, 255);padding-left:100px;" href="index.php"
+        <a class="navbar-brand" style="color:rgb(56, 164, 255);padding-left:400px;" href="index.php"
            onmouseover="this.style.color='rgb(0, 138, 255)';" onmouseout="this.style.color='rgb(56, 164, 255)'">MYSS</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,7 +26,7 @@
         </div>
 
         <div class="collapse navbar-collapse flex-grow-0 ml-auto" id="navbarSupportedContent">
-            <ul class="navbar-nav text-right" style="padding-right:100px;">
+            <ul class="navbar-nav text-right" style="padding-right:400px;">
                 <li class="nav-item">
                     <a class="nav-link" href="#" onmouseover="this.style.color='rgb(56, 164, 255)'"
                        onmouseout="this.style.color='';">
