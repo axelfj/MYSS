1cebd8b6bf62b779fb4b88e9ed8d4d721207f630
Axel Fernández <axl1210@estudiantec.cr>
2019-04-06 12:19:57 -0600

    Create Collection
    Se añaden colecciones para crear las conexiones entre nodos, estas conexiones son:
    - Usuario con los posts.
    - Posts con los tags.
    - Friends (userxuser).
    - Comentarios (postxpost).

--- myss/Controller/createCollection.php
+++ myss/Controller/createCollection.php
@@ -7,7 +7,6 @@ require_once("connection.php");
 $connection = connect();
 $collectionHandler = new ArangoCollectionHandler($connection);
 
-
 if ($collectionHandler->has('user')) {
     $collectionHandler->drop('user');
 }
@@ -16,6 +15,18 @@ if ($collectionHandler->has('posts')) {
 }
 if ($collectionHandler->has('tag')) {
     $collectionHandler->drop('tag');
+}
+if ($collectionHandler->has('comment')) {
+    $collectionHandler->drop('comment');
+}
+if ($collectionHandler->has('friends')) {
+    $collectionHandler->drop('friends');
+}
+if ($collectionHandler->has('userXpost')) {
+    $collectionHandler->drop('userXpost');
+}
+if ($collectionHandler->has('postXtag')) {
+    $collectionHandler->drop('postXtag');
 }
 
 // create the User collection //
@@ -33,4 +44,25 @@ $tagCollection = new ArangoCollection();
 $tagCollection->setName('tag');
 $id = $collectionHandler-> create($tagCollection);
 
+// create the Comment collection //
+$commentCollection = new ArangoCollection();
+$commentCollection->setName('comment');
+$id = $collectionHandler-> create($commentCollection);
 
+// create the userXuser collection //
+$userXuserCollection = new ArangoCollection();
+$userXuserCollection->setName('userXuser');
+$id = $collectionHandler->create($userXuserCollection);
+
+// create the userXpost collection //
+$userXpostCollection = new ArangoCollection();
+$userXpostCollection->setName('userXpost');
+$id = $collectionHandler->create($userXpostCollection);
+
+// create the postXtag collection //
+$postXtagCollection = new ArangoCollection();
+$postXtagCollection->setName('postXtag');
+$id = $collectionHandler->create($postXtagCollection);
+
+
+echo 'Se han creado las colecciones correctamente.';
