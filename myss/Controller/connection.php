beee92ce75e872378c5829422c08e7f59f3bee73
Axel Fernández <axl1210@estudiantec.cr>
2019-04-07 19:24:10 -0600

    Controller Finish

--- myss/Controller/connection.php
+++ myss/Controller/connection.php
@@ -16,34 +16,21 @@ use ArangoDBClient\ServerException as ArangoServerException;
 use ArangoDBClient\Statement as ArangoStatement;
 use ArangoDBClient\UpdatePolicy as ArangoUpdatePolicy;
 
-// set up some basic connection options
-
-// turn on exception logging (logs to whatever PHP is configured)
 ArangoException::enableLogging();
 
 function connect(){
     try{
         $connectionOptions = [
-            // database name
-            ArangoConnectionOptions::OPTION_DATABASE => 'myss',
-            // server endpoint to connect to
-            ArangoConnectionOptions::OPTION_ENDPOINT => 'tcp://127.0.0.1:8529',
-            // authorization type to use (currently supported: 'Basic')
-            ArangoConnectionOptions::OPTION_AUTH_TYPE => 'Basic',
-            // user for basic authorization
-            ArangoConnectionOptions::OPTION_AUTH_USER => 'root',
-            // password for basic authorization
-            ArangoConnectionOptions::OPTION_AUTH_PASSWD => '1234',
-            // connection persistence on server. can use either 'Close' (one-time connections) or 'Keep-Alive' (re-used connections)
-            ArangoConnectionOptions::OPTION_CONNECTION => 'Keep-Alive',
-            // connect timeout in seconds
-            ArangoConnectionOptions::OPTION_TIMEOUT => 3,
-            // whether or not to reconnect when a keep-alive connection has timed out on server
-            ArangoConnectionOptions::OPTION_RECONNECT => true,
-            // optionally create new collections when inserting documents
-            ArangoConnectionOptions::OPTION_CREATE => true,
-            // optionally create new collections when inserting documents
-            ArangoConnectionOptions::OPTION_UPDATE_POLICY => ArangoUpdatePolicy::LAST,
+            ArangoConnectionOptions::OPTION_DATABASE => 'myss', // database name
+            ArangoConnectionOptions::OPTION_ENDPOINT => 'tcp://127.0.0.1:8529', // server endpoint to connect to
+            ArangoConnectionOptions::OPTION_AUTH_TYPE => 'Basic', // authorization type to use (currently supported: 'Basic')
+            ArangoConnectionOptions::OPTION_AUTH_USER => 'root', // user for basic authorization
+            ArangoConnectionOptions::OPTION_AUTH_PASSWD => '1234', // password for basic authorization
+            ArangoConnectionOptions::OPTION_CONNECTION => 'Keep-Alive', // connection persistence on server. can use either 'Close' (one-time connections) or 'Keep-Alive' (re-used connections)
+            ArangoConnectionOptions::OPTION_TIMEOUT => 3, // connect timeout in seconds
+            ArangoConnectionOptions::OPTION_RECONNECT => true, // whether or not to reconnect when a keep-alive connection has timed out on server
+            ArangoConnectionOptions::OPTION_CREATE => true, // optionally create new collections when inserting documents
+            ArangoConnectionOptions::OPTION_UPDATE_POLICY => ArangoUpdatePolicy::LAST, // optionally create new collections when inserting documents
         ];
         $connection = new ArangoConnection($connectionOptions);
     } catch (Exception $exception){
