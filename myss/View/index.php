95ea97a0c131df5acba551afa4c51634fe905139
Axel Fernández <axl1210@estudiantec.cr>
2019-04-07 23:07:45 -0600

    Update index.php

--- myss/View/index.php
+++ myss/View/index.php
@@ -17,7 +17,7 @@ $document = new ArangoCollectionHandler(connect());
 ?>
 
 <!--================ Start Blog Post Area =================-->
-<section class="blog-post-area ">
+<section class="blog-post-area">
     <div class="container">
         <div class="row">
             <div class="col-md-8">
@@ -31,39 +31,32 @@ $document = new ArangoCollectionHandler(connect());
             <!-- Start Blog Post Sidebar -->
             <div class="col-md-4 sidebar-widgets">
                 <div class="widget-wrap">
-
-
-                    <div class="single-sidebar-widget post-category-widget">
+                    <div class="single-sidebar-widget post-category-widget" style="background: white">
                         <h4 class="ti-tag"> Popular Tags </h4>
                         <ul class="cat-list mt-20">
                             <li>
                                 <a href="#" class="d-flex justify-content-between">
-                                    <p>Fashion</p>
-                                    <p>59</p>
+                                    <p>Fashion</p><p>59</p>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="d-flex justify-content-between">
-                                    <p>Travel</p>
-                                    <p>09</p>
+                                    <p>Travel</p><p>09</p>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="d-flex justify-content-between">
-                                    <p>Lifestyle</p>
-                                    <p>24</p>
+                                    <p>Lifestyle</p><p>24</p>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="d-flex justify-content-between">
-                                    <p>Shopping</p>
-                                    <p>44</p>
+                                    <p>Shopping</p><p>44</p>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="d-flex justify-content-between">
-                                    <p>Food</p>
-                                    <p>15</p>
+                                    <p>Food</p><p>15</p>
                                 </a>
                             </li>
                         </ul>
