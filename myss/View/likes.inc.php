17db2f9cbb6ab450bc01e08e4307b961ecccd07b
Axel Fernández <axl1210@estudiantec.cr>
2019-04-13 17:47:32 -0600

    Update likes.inc.php
    The same user can't like the same post, twice.

--- myss/View/likes.inc.php
+++ myss/View/likes.inc.php
@@ -5,48 +5,52 @@ require_once "../Controller/arangodb-php/lib/ArangoDBClient/CollectionHandler.ph
 require_once "../Controller/arangodb-php/lib/ArangoDBClient/Cursor.php";
 require_once "../Controller/arangodb-php/lib/ArangoDBClient/DocumentHandler.php";
 require_once "../Controller/createEdges.php";
+require_once "../Controller/readCollection.php";
 
 session_start();
 
-// Gets the url and finds the comment button that was pressed.
 use ArangoDBClient\CollectionHandler as ArangoCollectionHandler;
+use function ArangoDBClient\readCollection;
 use ArangoDBClient\Statement as ArangoStatement;
 
-// Gets the key of the post that is getting commented.
+
 $url = $_SERVER['REQUEST_URI'];
 $posStart = strpos($url, '@') + 1;
 $posEnd = strlen($url);
 $postKey = substr($url, $posStart, $posEnd - $posStart);
-
-$database = connect();
-$document = new ArangoCollectionHandler(connect());
-
-$cursor = $document->byExample('post', ['visibility' => "Public"], ['visibility' => "Private"]);
-$valueFound = $cursor->getCount();
-
-if ($valueFound != 0) {
-
-
-    $query = 'FOR x IN post FILTER x._key == @var UPDATE { _key: x._key, likes: x.likes+1} IN post';
-
-    $statement = new ArangoStatement(
-        $database,
-        array(
-            "query" => $query,
-            "count" => true,
-            "batchSize" => 1,   // It is suppose to only bring one.
-            "sanitize" => true,
-            "bindVars" => array("var" => $postKey)
-        )
-    );
-
-    $cursor = $statement->execute();
-    createEdge('user', $_SESSION['userKey'], 'post', $postKey, 'liked');
-}
-
 $pos = strpos($url, 'profile.php');
 if ($pos == false) {
     header('Location: index.php');
 } else {
     header('Location: profile.php');
+}
+try {
+    $database = connect();
+    $document = new ArangoCollectionHandler(connect());
+
+    $cursor = $document->byExample('post', ['visibility' => "Public"], ['visibility' => "Private"]);
+
+    if ($cursor->getCount() != 0) {
+        $statements = [
+            'FOR u in liked 
+            FILTER u._to == @postKey && u._from == @userKey 
+            RETURN u'
+            => ['postKey' => $postKey, 'userKey' => $_SESSION['userKey']]];
+        $liked = readCollection($statements);
+        var_dump($liked);
+
+        if ($liked->getCount() == 0) {
+
+            $statements = [$query = 'FOR x IN post FILTER x._key == @postKey UPDATE { _key: x._key, likes: x.likes+1} IN post' => ['postKey' => $postKey]];
+            readCollection($statements);
+
+            createEdge('user', $_SESSION['userKey'], 'post', $postKey, 'liked');
+        }
+        else{
+            // delete the edge //
+        }
+    }
+
+} catch (Exception $e) {
+    echo $e->getMessage();
 }
