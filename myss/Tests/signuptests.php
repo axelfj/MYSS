0af7b55caaac8c8a0631898908b5b0f3831e211b
Brends31 <aobm98@gmail.com>
2019-04-15 23:24:15 -0600

    Adición de clases de prueba y .phar
    Se crea una clase para probar el signup y se agrega el phpunit.phar

---
+++ myss/Tests/signuptests.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Simastir_da
+ * Date: 15/04/2019
+ * Time: 23:19
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class signuptests extends TestCase
+{
+
+
+
+}
