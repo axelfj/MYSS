573d5872ffa48a399fc2142faccb2bb94ee8c6ee
Axel Fernández <axl1210@estudiantec.cr>
2019-06-13 21:08:02 -0600

    Update fb-init.php

--- View/fb-init.php
+++ View/fb-init.php
@@ -12,39 +12,42 @@ require "../vendor/autoload.php";
  * → The information in the array is provided by Facebook developers.
  * → It has a limit, don't overcast it.
  */
-$facebookAPI = new \Facebook\Facebook([
-   'app_id'                 => '476705966414394',
-   'app_secret'             => '801157a31bd6e8885a233ee1ffcbd06b',
-    'default_graph_version' => 'v2.10'
-]);
+try {
+    $facebookAPI = new \Facebook\Facebook([
+        'app_id' => '476705966414394',
+        'app_secret' => '801157a31bd6e8885a233ee1ffcbd06b',
+        'default_graph_version' => 'v2.10'
+    ]);
+} catch (\Facebook\Exceptions\FacebookSDKException $e) {
+}
 
-$helper     = $facebookAPI->getRedirectLoginHelper();
-$loginURL   = $helper->getLoginUrl('http://localhost/MYSS/myss/View/index.php'); // Redirects here.
+$helper = $facebookAPI->getRedirectLoginHelper();
+$loginURL = $helper->getLoginUrl('https://myss-qa.herokuapp.com/myss/View/index.php'); // Redirects here.
 
 // We set the userToken in the session. 
 $_SESSION['userToken'] = null;
 
-try{
+try {
 
     // Now, let's catch his token and save it.
     $userToken = $helper->getAccessToken();
-    if (isset($userToken)){
+    if (isset($userToken)) {
         $_SESSION['userToken'] = (string)$userToken;        // We put it in the session to have it "globally".
     }
 
-} catch (Exception $e){
+} catch (Exception $e) {
     echo $e->getTraceAsString();
 }
 
 // If we have his access token, then we can make an array with all of his information.
-if ($_SESSION['userToken'] != null){
-    try{
+if ($_SESSION['userToken'] != null) {
+    try {
 
         // We assign the token.
         $facebookAPI->setDefaultAccessToken($_SESSION['userToken']);
 
         // Asks for: name, birthday, his profile pic.
-        $queryResult        = $facebookAPI->get('/me?locale=en_US&fields=email');
+        $queryResult = $facebookAPI->get('/me?locale=en_US&fields=email');
 
         // Gets the information of the User.
         $userInformation = $queryResult->getGraphUser();
@@ -61,31 +64,29 @@ if ($_SESSION['userToken'] != null){
             $resultingDocuments = array();
             $profile = array();
             foreach ($userInformationCursor as $key => $value) {
-                $resultingDocuments[$key]   = $value;
-                $profile['username']        = $resultingDocuments[$key]->get('username');
-                $profile['name']            = $resultingDocuments[$key]->get('name');
-                $profile['email']           = $resultingDocuments[$key]->get('email');
-                $profile['key']             = $resultingDocuments[$key]->get('key');
-                $profile['userImage']       = $resultingDocuments[$key]->get('userImage');
+                $resultingDocuments[$key] = $value;
+                $profile['username'] = $resultingDocuments[$key]->get('username');
+                $profile['name'] = $resultingDocuments[$key]->get('name');
+                $profile['email'] = $resultingDocuments[$key]->get('email');
+                $profile['key'] = $resultingDocuments[$key]->get('key');
+                $profile['userImage'] = $resultingDocuments[$key]->get('userImage');
             }
 
             // We have his information, let's set it to the Session.
-            $_SESSION['username']   = $profile['username'];
-            $_SESSION['userKey']    = $profile['key'];
-            $_SESSION['name']       = $profile['name'];
-            $_SESSION['email']      = $profile['email'];
-            $_SESSION['userImage']  = $profile['userImage'];
+            $_SESSION['username'] = $profile['username'];
+            $_SESSION['userKey'] = $profile['key'];
+            $_SESSION['name'] = $profile['name'];
+            $_SESSION['email'] = $profile['email'];
+            $_SESSION['userImage'] = $profile['userImage'];
 
-            header('http://localhost/MYSS/myss/View/index.php');
-        }
-
-        // He's not in the database, let's redirect him to the register.
-        else{
+            header('http://myss-qa.herokuapp.com/myss/View/index.php');
+        } // He's not in the database, let's redirect him to the register.
+        else {
 
             // Send a message here.
         }
 
-    } catch (Exception $e){
+    } catch (Exception $e) {
         echo $e->getTraceAsString();
     }
 }
