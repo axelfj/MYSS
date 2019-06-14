b2e03cc3b6f447a278a1f94f1393c6075dd33474
Axel Fernández <axl1210@estudiantec.cr>
2019-06-13 20:34:36 -0600

    Update connection.php

--- Controller/connection.php
+++ Controller/connection.php
@@ -22,10 +22,10 @@ function connect(){
     try{
         $connectionOptions = [
             ArangoConnectionOptions::OPTION_DATABASE => 'myss', // database name
-            ArangoConnectionOptions::OPTION_ENDPOINT => 'tcp://127.0.0.1:8529', // server endpoint to connect to
+            ArangoConnectionOptions::OPTION_ENDPOINT => 'tcp://104.198.143.130:8529', // server endpoint to connect to
             ArangoConnectionOptions::OPTION_AUTH_TYPE => 'Basic', // authorization type to use (currently supported: 'Basic')
             ArangoConnectionOptions::OPTION_AUTH_USER => 'root', // user for basic authorization
-            ArangoConnectionOptions::OPTION_AUTH_PASSWD => '1234', // password for basic authorization
+            ArangoConnectionOptions::OPTION_AUTH_PASSWD => 'admin123*', // password for basic authorization
             ArangoConnectionOptions::OPTION_CONNECTION => 'Keep-Alive', // connection persistence on server. can use either 'Close' (one-time connections) or 'Keep-Alive' (re-used connections)
             ArangoConnectionOptions::OPTION_TIMEOUT => 3, // connect timeout in seconds
             ArangoConnectionOptions::OPTION_RECONNECT => true, // whether or not to reconnect when a keep-alive connection has timed out on server
