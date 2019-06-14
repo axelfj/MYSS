9cef800c657af73f667dd6894ecdc85ee03128bb
Jorge Barquero Villalobos <jorgebv02@gmail.com>
2019-06-14 01:43:21 -0600

    Update fb-callback.php

--- fb-callback.php
+++ fb-callback.php
@@ -73,80 +73,82 @@ if (! $userToken->isLongLived()) {
 
 $_SESSION['userToken'] = (string)$userToken;  // We put it in the session to have it "globally".
 // If we have his access token, then we can make an array with all of his information.
-if ($_SESSION['userToken'] != null) {
-    try {
-
-        /*
-        echo '<h3>TOKEN</h3>';
-        var_dump($_SESSION);
-        */
-        // We assign the token.
-        $facebookAPI->setDefaultAccessToken($_SESSION['userToken']);
-
-        /*echo '<h3>FACEBOOK API</h3>';
-        var_dump($facebookAPI);
-        */
-
-        // Asks for: name, birthday, his profile pic.
-        $queryResult = $facebookAPI->get('/me?locale=en_US&fields=email');
-
-        /*echo '<h3>QUERY RESULT</h3>';
-        var_dump($queryResult);*/
-
-        // Gets the information of the User.
-        $userInformation = $queryResult->getGraphUser();
-
-        /*echo '<h3>USER INFORMATION</h3>';
-        var_dump($userInformation);*/
-
-        // We save this email.
-        $facebookEmailProvidedByAPI = $userInformation->getField('email');
-
-        /*echo '<h3>FACEBOOK EMAiL PROVIDED</h3>';
-        var_dump($facebookEmailProvidedByAPI);*/
-
-        // Now, let's see if he's in the database.
-        // $controller = new Controller();
-
-        header('location: https://myss-qa.herokuapp.com/View/index.php');
-
-        /*
-        if ($controller->isEmailTaken($facebookEmailProvidedByAPI) == true) {
-
-            // We gather his information and set it in the $_SESSION.
-            $userInformationCursor = $controller->getUser($facebookEmailProvidedByAPI);
-            $resultingDocuments = array();
-            $profile = array();
-            foreach ($userInformationCursor as $key => $value) {
-                $resultingDocuments[$key] = $value;
-                $profile['username'] = $resultingDocuments[$key]->get('username');
-                $profile['name'] = $resultingDocuments[$key]->get('name');
-                $profile['email'] = $resultingDocuments[$key]->get('email');
-                $profile['key'] = $resultingDocuments[$key]->get('key');
-                $profile['userImage'] = $resultingDocuments[$key]->get('userImage');
-            }
-
-            // We have his information, let's set it to the Session.
-            $_SESSION['username'] = $profile['username'];
-            $_SESSION['userKey'] = $profile['key'];
-            $_SESSION['name'] = $profile['name'];
-            $_SESSION['email'] = $profile['email'];
-            $_SESSION['userImage'] = $profile['userImage'];
 
 
-
-            header('location: https://myss-qa.herokuapp.com/View/index.php');
-
-        } // He's not in the database, let's redirect him to the register.
-        else {
-            header('location: https://myss-qa.herokuapp.com/View/login.php');
-            // Send a message here.
-        }
-        */
-
-    } catch (Exception $e) {
-        echo $e->getTraceAsString();
-    }
-}
+//if ($_SESSION['userToken'] != null) {
+//    try {
+//
+//        /*
+//        echo '<h3>TOKEN</h3>';
+//        var_dump($_SESSION);
+//        */
+//        // We assign the token.
+//        $facebookAPI->setDefaultAccessToken($_SESSION['userToken']);
+//
+//        /*echo '<h3>FACEBOOK API</h3>';
+//        var_dump($facebookAPI);
+//        */
+//
+//        // Asks for: name, birthday, his profile pic.
+//        $queryResult = $facebookAPI->get('/me?locale=en_US&fields=email');
+//
+//        /*echo '<h3>QUERY RESULT</h3>';
+//        var_dump($queryResult);*/
+//
+//        // Gets the information of the User.
+//        $userInformation = $queryResult->getGraphUser();
+//
+//        /*echo '<h3>USER INFORMATION</h3>';
+//        var_dump($userInformation);*/
+//
+//        // We save this email.
+//        $facebookEmailProvidedByAPI = $userInformation->getField('email');
+//
+//        /*echo '<h3>FACEBOOK EMAiL PROVIDED</h3>';
+//        var_dump($facebookEmailProvidedByAPI);*/
+//
+//        // Now, let's see if he's in the database.
+//        // $controller = new Controller();
+//
+//        header('location: https://myss-qa.herokuapp.com/View/index.php');
+//
+//        /*
+//        if ($controller->isEmailTaken($facebookEmailProvidedByAPI) == true) {
+//
+//            // We gather his information and set it in the $_SESSION.
+//            $userInformationCursor = $controller->getUser($facebookEmailProvidedByAPI);
+//            $resultingDocuments = array();
+//            $profile = array();
+//            foreach ($userInformationCursor as $key => $value) {
+//                $resultingDocuments[$key] = $value;
+//                $profile['username'] = $resultingDocuments[$key]->get('username');
+//                $profile['name'] = $resultingDocuments[$key]->get('name');
+//                $profile['email'] = $resultingDocuments[$key]->get('email');
+//                $profile['key'] = $resultingDocuments[$key]->get('key');
+//                $profile['userImage'] = $resultingDocuments[$key]->get('userImage');
+//            }
+//
+//            // We have his information, let's set it to the Session.
+//            $_SESSION['username'] = $profile['username'];
+//            $_SESSION['userKey'] = $profile['key'];
+//            $_SESSION['name'] = $profile['name'];
+//            $_SESSION['email'] = $profile['email'];
+//            $_SESSION['userImage'] = $profile['userImage'];
+//
+//
+//
+//            header('location: https://myss-qa.herokuapp.com/View/index.php');
+//
+//        } // He's not in the database, let's redirect him to the register.
+//        else {
+//            header('location: https://myss-qa.herokuapp.com/View/login.php');
+//            // Send a message here.
+//        }
+//        */
+//
+//    } catch (Exception $e) {
+//        echo $e->getTraceAsString();
+//    }
+//}
 
 
